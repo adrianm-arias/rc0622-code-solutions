bac51e4bbd0eeaf9278e7e3ba210928940cb5fd5
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-05 18:13:40 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'Adrian Arias';
+console.log('Value of fullName:', fullName);
+var isCool = true;
+console.log('Value of isCool:', isCool);
+var totalPets = 2;
+console.log('Value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+var never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
