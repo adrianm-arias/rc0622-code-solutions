4a566328ae27fd505832445051b2d9f0abf126fb
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-05 19:32:52 +0000

    completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,42 @@
+var width = 5;
+var height = 7;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 145;
+var payment = 200;
+var change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 77;
+var midterm = 87;
+var final = 97;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Adrian';
+var lastName = 'Arias';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var ph = 5;
+var isAcidic = ph < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headcount = 17;
+var isSparta = headcount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
