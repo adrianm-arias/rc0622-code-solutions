ac3fa61c1a4aa25575aa703874a704b270adc206
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-06 20:11:04 +0000

    completed javascript-functions-practice exercise

---
+++ javascript-functions-practice/get-character-at-index.js
@@ -0,0 +1,6 @@
+/* exported getCharacterAtIndex */
+/* exported getSecondCharacter */
+function getCharacterAtIndex(string, index) {
+  var characterAtIndex = string.charAt(index);
+  return characterAtIndex;
+}
