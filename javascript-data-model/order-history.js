7304f23425d9922f7ecab4aa1447d30b985627f6
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-06 01:42:05 +0000

    completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,56 @@
+var orderHistory = [
+  {
+    orderPlace: 'August 4, 2020',
+    totalPrice: 34.00,
+    shipTo: 'JS Masher',
+    orderId: '114-3941689 - 8772232',
+    delivered: 'Aug 8, 2020',
+    item: 'JavaScript for impatient programmers',
+    itemAuthor: 'Rauschmayer, Dr. Axel',
+    returnWindow: 'Sep 7, 2020',
+    itemPrice: 31.55
+  },
+  {
+    orderPlace: 'July 19, 2020',
+    totalPrice: 44.53,
+    shipTo: 'JS Masher',
+    orderId: '113-9984268-1280257',
+    delivered: 'Jul 20, 2020',
+    item: 'The Timeless Way of Building',
+    itemAuthor: 'Alexander, Christopher',
+    returnWindow: 'Aug 19, 2020',
+    itemPrice: 41.33
+  },
+  {
+    orderPlace: 'July 4, 2020',
+    totalPrice: 17.22,
+    shipTo: 'JS Masher',
+    orderId: '114-2875557-9059409',
+    delivered: 'Jul 7, 2020',
+    item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WI U&comma;PC.Support Turbo and Vibration Features.No Driver and No Lag- Gamecube Adapter',
+    itemAuthor: null,
+    returnWindow: 'Aug 5, 2020',
+    itemPrice: 15.98
+  },
+  {
+    orderPlace: 'July 3, 2020',
+    totalPrice: 138.93,
+    shipTo: 'JS Masher',
+    orderId: '113-2883177-2648248',
+    delivered: 'Jul 5, 2020',
+    item: [
+      {
+        itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        itemAuthor: null,
+        returnWindow: 'Aug 4, 2020',
+        itemPrice: 94.95
+      },
+      {
+        itemName: 'The Art of Sal',
+        itemAuthor: 'Faroult, Stephane',
+        returnWindow: 'Aug 4, 2020',
+        itemPrice: 33.99
+      }
+    ]
+  }
+];
