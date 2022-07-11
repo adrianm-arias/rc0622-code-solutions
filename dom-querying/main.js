20db19fdd39d3589ef5a247bd00d0d4379fb3e61
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-11 17:49:17 +0000

    completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var elementH1 = document.querySelector('h1');
+console.log('$heading', elementH1);
+console.dir(elementH1);
+
+var idExplanation = document.querySelector('#explanation');
+console.log('$explanation', idExplanation);
+console.dir(idExplanation);
+
+var classHint = document.querySelector('.hint');
+console.log('$hint', classHint);
+console.dir(classHint);
+
+var tagP = document.querySelectorAll('p');
+console.log('$paragraphs', tagP);
+
+var allExampleLinks = document.querySelectorAll('.example-link');
+console.log('$links', allExampleLinks);
