0d65002bf76d8e3ef540fdece933c20863b6fc36
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-16 01:11:51 +0000

    in progress

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,18 @@
 /* exported lastChars */
+// check and index characters of string
+// add characters to new string if the index is number is more than the length parameter
+// return new string
+
+function lastChars(length, string) {
+  var shortString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i > (string.length - 1) - length) {
+      shortString += string[i];
+    }
+  }
+  return shortString;
+}
+
+// checked and indexed characters of string with for loop
+// added characters to new string if the index is number is more string's lenght - the length parameter
+// returned new string
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,23 @@
 /* exported numVowels */
+// lowercase string
+// index the characters of a string
+// check if any of those characters are vowels
+// add vowel characters to new string
+// return string length of vowel string to see how many vowels in selected string
+
+function numVowels(string) {
+  var str = string.toLowerCase();
+  var newString = '';
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
+      newString += str[i];
+    }
+  }
+  return newString.length;
+}
+
+// lowercased string
+// indexed the characters of a string
+// checked if any of those characters are vowels
+// added vowel characters to new string
+// returned string length of vowel string to see how many vowels in selected string
