975a4d06d512ff4393ebd10ee3fcaf07147d89ff
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-22 16:08:42 +0000

    in progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,42 @@
 /* exported omit */
+
+// function omit(source, keys) {
+//   var newObject = {};
+//   var keyOne = keys[0];
+//   var keyTwo = keys[1];
+//   // console.log(keyOne);
+//   // console.log(keyTwo);
+//   // var entries = Object.entries(source);
+//   for (var property in source) {
+//     if (keyOne !== property && keyTwo !== property) {
+//       console.log('prop:', property);
+//       console.log('value:', source[property]);
+//       newObject.bar = source[property];
+//     }
+//   }
+//   console.log(newObject);
+// }
+
+// function toObject(keyValuePair) {
+//   var newObject = {};
+//   for (let i = 0; i < keyValuePair.length; i++) {
+//     newObject[keyValuePair[0]] = keyValuePair[1];
+//   }
+//   return newObject;
+// }
+
+// function getValues(object) {
+//   var objectArray = [];
+//   for (var property in object) {
+//     objectArray.push(object[property]);
+//   }
+//   return objectArray;
+// }
+
+// function getKeys(object) {
+//   var objectArray = [];
+//   for (var property in object) {
+//     objectArray.push(property);
+//   }
+//   return objectArray;
+// }
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,12 +1,22 @@
 /* exported pick */
-// turn
+// index keys parameter
+// make sure source parameter object keys are not undefined
+// check is keys parameter is equal to keys in source object
+// if it is add key/value from source to new object
+// return new object
 
 function pick(source, keys) {
   var result = {};
-  for (var prop of keys) {
-    if (source[keys[prop]] !== undefined) {
-      result[keys[prop]] = source[keys[prop]];
+  for (var i = 0; i < keys.length; i++) {
+    if (source[keys[i]] !== undefined) {
+      result[keys[i]] = source[keys[i]];
     }
   }
   return result;
 }
+
+// indexed keys parameter for loop
+// make sure source parameter object keys are not undefined
+// checked is keys parameter is equal to keys in source object using an if statement
+// if it is add key/value from source to new object
+// returned new object
