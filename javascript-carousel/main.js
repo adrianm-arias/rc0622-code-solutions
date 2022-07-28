599757cb7f7105b077e50bad4088bd8d90a603f9
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-28 01:24:00 +0000

    slimmed down dots function.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -48,12 +48,6 @@ function timerReset() {
   timer = setInterval(carousel, 3000);
 }
 
-var $dotOne = document.getElementById('dot-one');
-var $dotTwo = document.getElementById('dot-two');
-var $dotThree = document.getElementById('dot-three');
-var $dotFour = document.getElementById('dot-four');
-var $dotFive = document.getElementById('dot-five');
-
 document.addEventListener('click', function (event) {
   if (event.target.matches('.fa-circle')) {
     if (event.target.id === 'dot-one') {
@@ -83,39 +77,16 @@ document.addEventListener('click', function (event) {
       return setImg();
     }
   }
-  // timerReset();
 });
 
+var $allDots = document.querySelectorAll('.dot');
+
 function dots() {
-  if (counter === 0) {
-    $dotOne.className = 'fa-solid fa-circle';
-    $dotTwo.className = 'fa-regular fa-circle';
-    $dotThree.className = 'fa-regular fa-circle';
-    $dotFour.className = 'fa-regular fa-circle';
-    $dotFive.className = 'fa-regular fa-circle';
-  } else if (counter === 1) {
-    $dotOne.className = 'fa-regular fa-circle';
-    $dotTwo.className = 'fa-solid fa-circle';
-    $dotThree.className = 'fa-regular fa-circle';
-    $dotFour.className = 'fa-regular fa-circle';
-    $dotFive.className = 'fa-regular fa-circle';
-  } else if (counter === 2) {
-    $dotOne.className = 'fa-regular fa-circle';
-    $dotTwo.className = 'fa-regular fa-circle';
-    $dotThree.className = 'fa-solid fa-circle';
-    $dotFour.className = 'fa-regular fa-circle';
-    $dotFive.className = 'fa-regular fa-circle';
-  } else if (counter === 3) {
-    $dotOne.className = 'fa-regular fa-circle';
-    $dotTwo.className = 'fa-regular fa-circle';
-    $dotThree.className = 'fa-regular fa-circle';
-    $dotFour.className = 'fa-solid fa-circle';
-    $dotFive.className = 'fa-regular fa-circle';
-  } else if (counter === 4) {
-    $dotOne.className = 'fa-regular fa-circle';
-    $dotTwo.className = 'fa-regular fa-circle';
-    $dotThree.className = 'fa-regular fa-circle';
-    $dotFour.className = 'fa-regular fa-circle';
-    $dotFive.className = 'fa-solid fa-circle';
+  for (var i = 0; i < $allDots.length; i++) {
+    if (counter === i) {
+      $allDots[i].className = 'dot fa-solid fa-circle';
+    } else {
+      $allDots[i].className = 'dot fa-regular fa-circle';
+    }
   }
 }
