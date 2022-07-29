2586e4d68128d17a775b3e5b3afdf3da0bcb4b5d
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-29 00:51:28 +0000

    completed array-manipulation-3 exercises.

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,22 @@
 /* exported difference */
+// check and index first array
+// check and index second array
+// compare both arrays index by index
+// if they match do nothing
+// if they don't add them to new array
+// return new array
+
+function difference(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (!second.includes(first[i])) {
+      newArray.push(first[i]);
+    }
+  }
+  for (var y = 0; y < second.length; y++) {
+    if (!first.includes(second[y])) {
+      newArray.push(second[y]);
+    }
+  }
+  return newArray;
+}
--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,17 @@
 /* exported intersection */
+// check and index first array
+// check and index second array
+// compare both arrays index by index
+// if items in array 1 match array 2 if so add to new array
+// if not do nothing
+// return new array
+
+function intersection(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      newArray.push(first[i]);
+    }
+  }
+  return newArray;
+}
