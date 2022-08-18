e4935d87675b84a0fd4d05c921f8cc4d72eff423
Adrian Arias <adrianarias.pro@gmail.com>
2022-08-18 19:51:27 +0000

    working on level 3 challenge

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,8 +1,16 @@
 const argv = process.argv.slice(2);
 const fs = require('fs');
 
-for (const arg of argv) {
-  fs.readFile(arg, 'utf8', (err, data) => {
+// for (const arg of argv) {
+//   fs.readFile(arg, 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//   });
+// }
+
+for (let i = 0; i < argv.length; i++) {
+  console.log(argv[i]);
+  fs.readFile(argv[i], 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
   });
