0e495496bb0c7540225d0c4283cea8046104a250
Adrian Arias <adrianarias.pro@gmail.com>
2022-08-19 02:07:49 +0000

    completed level 3 challenege

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,17 +1,19 @@
 const argv = process.argv.slice(2);
 const fs = require('fs');
 
-// for (const arg of argv) {
-//   fs.readFile(arg, 'utf8', (err, data) => {
-//     if (err) throw err;
-//     console.log(data);
-//   });
-// }
-
-for (let i = 0; i < argv.length; i++) {
-  console.log(argv[i]);
-  fs.readFile(argv[i], 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
+let counter = 0;
+function readAll() {
+  if (counter < argv.length) {
+    const file = argv[counter];
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      console.log(data);
+      counter++;
+      readAll();
+    });
+  }
 }
+readAll();
