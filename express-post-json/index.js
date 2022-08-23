17cb5880b005d22ce5ac91e0c528efd0c0067f33
Adrian Arias <adrianarias.pro@gmail.com>
2022-08-23 16:26:17 +0000

    fixed by moving array inside the get req callback function and fixed new grade post req to be added to grades object

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -3,25 +3,25 @@ const app = express();
 
 let nextId = 1;
 const grades = {};
-const newArray = [];
 app.get('/api/grades', (req, res) => {
+  const newArray = [];
   for (const prop in grades) {
     newArray.push(grades[prop]);
   }
   res.json(newArray);
-})
+});
 
 app.use(express.json());
 
 app.post('/api/grades', (req, res) => {
   const newGrade = req.body;
   newGrade.id = nextId;
+  grades[newGrade.id] = newGrade;
   nextId++;
-  newArray.push(newGrade);
   res.status(201).json(newGrade);
-})
+});
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('listening on port 3000');
-})
+});
