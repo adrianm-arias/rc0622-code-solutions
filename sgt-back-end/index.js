1b45905e1e5f77919b51ec3c474b05c64cba9e7e
Adrian Arias <adrianarias.pro@gmail.com>
2022-08-29 15:20:29 +0000

    completed sgt-back-end exercise

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-// const fs = require('fs');
 const pg = require('pg');
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
