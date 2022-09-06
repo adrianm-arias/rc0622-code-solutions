d28e0b5de80c07ed0021c6d793e98bfbcb929e82
Adrian Arias <adrianarias.pro@gmail.com>
2022-09-06 23:44:55 +0000

    completed react-app-drawer-component exercises.

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -18,31 +18,23 @@ class AppDrawer extends React.Component {
   render() {
     if (!this.state.toggleOn) {
       return (
-        <div className='nav-wrapper'>
-          <i className="fa-solid fa-bars menu-icon" onClick={this.handleClick}></i>
+        <div className='bg' onClick={this.handleClick}>
+          <nav className=''>
+            <i className="fa-solid fa-bars menu-icon" onClick={this.handleClick}></i>
+          </nav>
         </div>
       );
     } else {
       return (
-        <div className='bg-overlay'>
-          <div className='nav-wrapper'>
-            <div>
-              <a href="#" className='nav-logo' onClick={this.handleClick}>MENU</a>
-            </div>
-            <div>
-              <ul className='nav-link-wrapper'>
-                <a href="#" className='nav-link' onClick={this.handleClick}>
-                  <li className='nav'>About</li>
-                </a>
-                <a href="#" className='nav-link' onClick={this.handleClick}>
-                  <li className='nav'>Get Started</li>
-                </a>
-                <a href="#" className='nav-link' onClick={this.handleClick}>
-                  <li className='nav'>Sign In</li>
-                </a>
-              </ul>
-            </div>
-          </div>
+        <div className='bg-overlay' onClick={this.handleClick}>
+          <nav className='nav-wrapper'>
+            <h1 className='nav-logo' onClick={this.handleClick}>MENU</h1>
+            <ul className='nav-link-wrapper'>
+              <li className='nav-link' onClick={this.handleClick}>About</li>
+              <li className='nav-link' onClick={this.handleClick}>Get Started</li>
+              <li className='nav-link' onClick={this.handleClick}>Sign In</li>
+            </ul>
+          </nav>
         </div>
       );
     }
